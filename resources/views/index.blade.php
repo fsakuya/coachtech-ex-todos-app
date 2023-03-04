2a11f062e97bec024f4bdea89e7c881071740617
fsakuya <futamori.1991@gmail.com>
2023-03-04 18:11:03 +0900

    commit

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,7 +5,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-widthz, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>COACHTECH</title>
+  <title>Todos</title>
   <link rel="stylesheet" href="css/reset.css" />
   <style>
     html,
